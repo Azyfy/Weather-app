2fdcbfbee80f01332c43bcabfa2cbcb01e70cf98
Azzryel <AzryelShroud@gmail.com>
2021-01-29 17:42:56 +0100

    add option for units, convert and display them

--- dist/main.js
+++ dist/main.js
@@ -17,29 +17,37 @@ function getWeather (city) {
           Switched to do it with async await. 
     */
    
-   async function getWeather(city) {
-     let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3ea0f850bb5508c08514a8d1e9523f48`, {mode: 'cors'});
+   async function getWeather(city, temp) {
+     let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}${temp}&APPID=3ea0f850bb5508c08514a8d1e9523f48`, {mode: 'cors'});
      let weather = await response.json();
      // !!response.ok do error later
 
      console.log("asy");
-     fillTemplate (weather);
+     fillTemplate (weather, temp);
      console.log(weather);
    }
    console.log("this weather");
 
-   function fillTemplate (weather) {
+   function fillTemplate (weather, temp) {
       let city = document.querySelector("#city");
       let country = document.querySelector("#country");
       let weatherDesc = document.querySelector("#weather");
       let temperature = document.querySelector("#temperature");
       let humidity = document.querySelector("#humidity");
 
+      let deg = " °K";
+      if (temp == "&units=metric") {
+        deg = " °C";
+      }
+      else if (temp == "&units=imperial") {
+        deg = " °F";
+      }
+
       city.innerText = `${weather.name}, `;
       country.innerText = `${weather.sys.country}`;
       weatherDesc.innerText = `${weather.weather[0].description}`;
-      temperature.innerText = `${weather.main.temp}`;
-      humidity.innerText = `${weather.main.humidity}`;
+      temperature.innerText = `${weather.main.temp}${deg}`;
+      humidity.innerText = `${weather.main.humidity} %`;
    }
 
     
@@ -47,11 +55,12 @@ function getWeather (city) {
 
 
 let searchBtn = document.querySelector("#search");
-console.log(searchBtn);
+let tempSelect = document.querySelector("#select-temp");
+
 searchBtn.addEventListener("keyup", (e) => {
     if (e.keyCode === 13) {
         //keyCode for enter
-    getWeather(searchBtn.value);
+    getWeather(searchBtn.value, tempSelect.value);
     searchBtn.value = "";
     };
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { getWeather } from './getWeather'
 
 let searchBtn = document.querySelector("#search");
-console.log(searchBtn);
+let tempSelect = document.querySelector("#select-temp");
+
 searchBtn.addEventListener("keyup", (e) => {
     if (e.keyCode === 13) {
         //keyCode for enter
-    getWeather(searchBtn.value);
+    getWeather(searchBtn.value, tempSelect.value);
     searchBtn.value = "";
     };
 });
